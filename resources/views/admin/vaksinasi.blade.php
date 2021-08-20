09054110b4cb24b42b0728a7431fa4394213b367
Zaky Thariq H <zakythariq@gmail.com>
2021-08-20 15:31:22 +0700

    jadwalvaksinupdate

--- resources/views/admin/vaksinasi.blade.php
+++ resources/views/admin/vaksinasi.blade.php
@@ -164,40 +164,6 @@
                   </a>
                 </div>    
                 @endforeach
-                  <div class="card-list-item">
-                    <a href="#">
-                      <div class="d-flex justify-content-between align-items-center sc-link">
-                        <div class="media">
-                          <div class="wd-40 ht-40 bg-its-icon tx-color-its mg-r-15 mg-md-r-15 d-flex align-items-center justify-content-center rounded-its"><i data-feather="calendar"></i></div>
-                          <div class="media-body align-self-center">
-                            <p class="tx-montserrat tx-semibold mg-b-0 tx-color-02">Sabtu, 24 Apr 2021</p> 
-                            <p class="tx-color-03 tx-13">08.00 - 15.00</p>
-                            <span class="tx-13"><span class="tx-gray-700"><i class="far fa-circle mg-r-5"></i>Pendaftaran belum dibuka</span></span>
-                          </div>
-                        </div>
-                        <div class="btn btn-icon btn-its-icon btn-hover">
-                          <i data-feather="chevron-right"></i>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                  <div class="card-list-item">
-                    <a href="#">
-                      <div class="d-flex justify-content-between align-items-center sc-link">
-                        <div class="media">
-                          <div class="wd-40 ht-40 bg-its-icon tx-color-its mg-r-15 mg-md-r-15 d-flex align-items-center justify-content-center rounded-its"><i data-feather="calendar"></i></div>
-                          <div class="media-body align-self-center">
-                            <p class="tx-montserrat tx-semibold mg-b-0 tx-color-02">Sabtu, 06 Mar 2021</p> 
-                            <p class="tx-color-03 tx-13">12.00 - 15.00</p>
-                            <span class="tx-13"><span class="tx-danger"><i class="far fa-times-circle mg-r-5"></i>Pendaftaran ditutup</span></span>
-                          </div>
-                        </div>
-                        <div class="btn btn-icon btn-its-icon btn-hover">
-                          <i data-feather="chevron-right"></i>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
                 </div>
               </div>
             </div>
